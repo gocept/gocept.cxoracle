9da792b34c8692a238d56a0458fbe3fb6b74064a
Christian Zagrodnick <cz@flyingcircus.io>
2008-08-29 10:01:27 +0000

    added a loader which sets the right environment for cx_Oracle

--- src/gocept/cxoracle/recipe.py
+++ src/gocept/cxoracle/recipe.py
@@ -1,11 +1,14 @@
 # Copyright (c) 2008 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import distutils.ccompiler
 import re
 import os
 import os.path
 import shutil
+import stat
 import subprocess
+import sys
 import tempfile
 
 
@@ -18,18 +21,27 @@ class CxOracle(object):
         self.buildout = buildout
         self.name = name
         self.options = options
-        self.part_directory = os.path.join(
+        self.part_directory = options['oracle-home'] = os.path.join(
             buildout['buildout']['parts-directory'],
             name)
+        options.setdefault('loader-name', name)
+        self.loader = options['_loader_path'] = os.path.join(
+            buildout['buildout']['bin-directory'], options['loader-name'])
+        options['executable'] = self.loader
 
     def install(self):
         if os.path.isdir(self.part_directory):
             shutil.rmtree(self.part_directory)
         os.mkdir(self.part_directory)
         self.prepare_oracle_home()
-        return self.part_directory
+        self.create_loader()
+        return self.part_directory, self.loader
+
+    def update(self):
+        pass
 
     def prepare_oracle_home(self):
+        """Prepare the part directory to contain all the libraries required."""
         self.unzip(self.options['instant-client'])
         self.unzip(self.options['instant-sdk'])
 
@@ -37,7 +49,7 @@ class CxOracle(object):
         for filename in os.listdir(self.part_directory):
             m = self.client_library_pattern.match(filename)
             if m is not None:
-                library_base_name = m.group(2)
+                library_base_name = m.group(1)
                 library_kind = m.group(2)
                 version = m.group(3)
                 symlink_source = filename
@@ -49,14 +61,42 @@ class CxOracle(object):
         os.symlink(os.path.join(self.part_directory, symlink_source),
                    os.path.join(self.part_directory, symlink_target))
 
+    def create_loader(self):
+        old_cwd = os.getcwd()
+        work_dir = tempfile.mkdtemp()
+        try:
+            os.chdir(work_dir)
+            shutil.copy(os.path.join(os.path.dirname(__file__), 'loader.c'),
+                        os.path.join(work_dir, 'loader.c'))
+
+            compiler = distutils.ccompiler.new_compiler()
+            compiler.define_macro('PYTHON_EXECUTABLE',
+                                  '"%s"' % sys.executable)
+            compiler.define_macro('ORACLE_HOME',
+                                  '"%s"' % self.options['oracle-home'])
+
+            compiler.compile(['loader.c'])
+            compiler.link_executable(['loader.o'], self.options['loader-name'])
+            shutil.move(os.path.join(work_dir, self.options['loader-name']),
+                        self.loader)
+        finally:
+            os.chdir(old_cwd)
+            shutil.rmtree(work_dir)
+
+
     def unzip(self, filename):
+        """Helper to unzip an archive to the parts directory."""
         extract_dir = tempfile.mkdtemp()
         try:
 
-            call = ['unzip', filename, '-d', extract_dir]
-            retcode = subprocess.call(call)
-            if retcode != 0:
-                raise Exception('Extraction of file %r failed' % extract_dir)
+            call = subprocess.Popen(
+                ['unzip', filename, '-d', extract_dir],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            stdout, stderr = call.communicate()
+
+            if call.returncode != 0:
+                raise Exception('Extraction of file %r failed.' % filename)
 
             contents = os.listdir(extract_dir)
             assert len(contents) == 1
