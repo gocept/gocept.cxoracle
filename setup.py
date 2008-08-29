c5fb6aac2d86dd85cb75e2a96b50af227b0517e1
Christian Zagrodnick <cz@flyingcircus.io>
2008-08-29 12:04:20 +0000

    added changes / fixed quotes in setup.py

--- setup.py
+++ setup.py
@@ -3,43 +3,46 @@
 
 from setuptools import setup, find_packages
 
-name = "gocept.cxoracle"
+name = 'gocept.cxoracle'
 
 classifiers = [
-    "Development Status :: 3 - Alpha"
-    "Environment :: Console",
-    "Environment :: Plugins",
-    "Framework :: Buildout"
-    "Intended Audience :: Developers"
-    "License :: OSI Approved :: Zope Public License"
-    "Operating System :: MacOS :: MacOS X"
-    "Operating System :: POSIX :: Linux"
-    "Programming Language :: C"
-    "Programming Language :: Python"
-    "Topic :: Software Development :: Build Tools",
-    "Topic :: System :: Software Distribution",
+    'Development Status :: 3 - Alpha'
+    'Environment :: Console',
+    'Environment :: Plugins',
+    'Framework :: Buildout'
+    'Intended Audience :: Developers'
+    'License :: OSI Approved :: Zope Public License'
+    'Operating System :: MacOS :: MacOS X'
+    'Operating System :: POSIX :: Linux'
+    'Programming Language :: C'
+    'Programming Language :: Python'
+    'Topic :: Software Development :: Build Tools',
+    'Topic :: System :: Software Distribution',
 ]
 
 setup(
     name = name,
-    version = "dev",
-    author = "Christian Zagrodnick",
-    author_email = "cz@gocept.com",
+    version = 'dev',
+    author = 'Christian Zagrodnick',
+    author_email = 'cz@gocept.com',
     description = \
-    "zc.buildout recipe for installing cx_Oracle",
+    'zc.buildout recipe for installing cx_Oracle',
     long_description = (
-        open("README.txt").read() + '\n\n' +
-        open(os.path.join('src', 'gocept', 'cxoracle', 'README.txt')).read()),
-    license = "ZPL 2.1",
+        open('README.txt').read() +
+        '\n\n' +
+        open(os.path.join('src', 'gocept', 'cxoracle', 'README.txt')).read() +
+        '\n\n' +
+        open('CHANGES.txt').read()),
+    license = 'ZPL 2.1',
     classifiers = classifiers,
-    url = "http://pypi.python.org/pypi/gocept.cxoracle",
-    download_ = "http://pypi.python.org/pypi/gocept.cxoracle",
-    packages = find_packages("src"),
+    url = 'http://pypi.python.org/pypi/gocept.cxoracle',
+    download_url = 'http://pypi.python.org/pypi/gocept.cxoracle',
+    packages = find_packages('src'),
     include_package_data = True,
-    package_dir = {"": "src"},
-    namespace_packages = ["gocept"],
-    install_requires = ["zc.buildout", "setuptools"],
-    extras_require = {"test": ["zope.testing"]},
-    entry_points = {"zc.buildout": ["default = %s.recipe:CxOracle" % name,],},
+    package_dir = {'': 'src'},
+    namespace_packages = ['gocept'],
+    install_requires = ['zc.buildout', 'setuptools'],
+    extras_require = {'test': ['zope.testing']},
+    entry_points = {'zc.buildout': ['default = %s.recipe:CxOracle' % name,],},
     )
 
